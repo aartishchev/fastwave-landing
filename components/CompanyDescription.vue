e6665e8a7bd526437a45d18e214f35edb5fbc16e
Артищев Алексей <artishev@satel.org>
2022-09-29 22:48:53 +0300

    make c

--- components/CompanyDescription.vue
+++ components/CompanyDescription.vue
@@ -4,49 +4,58 @@
       Компания FASTWA<span class="company-description__heading-highlight">V</span>E сегодня
     </h1>
 
-    <p class="company-description__definition">
-      FASTWAVE – логистическая компания,
-      которая предлагает комплексные решения по работе с поставщикамии доставке грузов из Китая
-    </p>
+    <div class="company-description__content-wrapper">
+      <p class="company-description__definition">
+        FASTWAVE – логистическая компания,
+        которая предлагает комплексные решения по работе с поставщикамии доставке грузов из Китая
+      </p>
+      <p class="company-description__story">
+        Мы начали бизнес в 2017 году с одной задачи -
+        предлагать индивидуальные решения в сфере международной логистики и ВЭД.
+        Со временем к нашему проекту присоединились специалисты с богатым опытом работы
+        с китайскими поставщиками и понимаем особенностей менталитета этой страны.
+        Мы развивались с ними и благодаря их экспертизе в 2021 году решили сфокусироваться
+        на доставке грузов из Китая, поиск поставщиков по этому направлению и развитие отношений с ними.
+        Работая с нами, Вы получаете поддержку команды профессионалов которые работают
+        в единой системе ценностей компании «Ответственность. Надежность. Оперативность»
+      </p>
+      <ul class="company-description__features-list">
+        <li class="company-description__feature-item">работаем более 5 лет</li>
+        <li class="company-description__feature-item">2 страны присутствия</li>
+        <li class="company-description__feature-item">говорим на 3 языках</li>
+        <li class="company-description__feature-item">c нами работать просто</li>
+      </ul>
+    </div>
 
-    <p class="company-description__story">
-      Мы начали бизнес в 2017 году с одной задачи -
-      предлагать индивидуальные решения в сфере международной логистики и ВЭД.
-      Со временем к нашему проекту присоединились специалисты с богатым опытом работы
-      с китайскими поставщиками и понимаем особенностей менталитета этой страны.
-      Мы развивались с ними и благодаря их экспертизе в 2021 году решили сфокусироваться
-      на доставке грузов из Китая, поиск поставщиков по этому направлению и развитие отношений с ними.
-      Работая с нами, Вы получаете поддержку команды профессионалов которые работают
-      в единой системе ценностей компании «Ответственность. Надежность. Оперативность»
-    </p>
-
-    <ul class="company-description__features-list">
-      <li class="company-description__feature-item">работаем более 5 лет</li>
-      <li class="company-description__feature-item">2 страны присутствия</li>
-      <li class="company-description__feature-item">говорим на 3 языках</li>
-      <li class="company-description__feature-item">c нами работать просто</li>
-    </ul>
-
-    <p class="company-description__offer">Читайте нас в VK</p>
-    <span class="company-description__about">работаем более 5 лет</span>
-    <hr class="company-description__divider">
-
-    <ul class="company-description__images-list">
-      <li v-for="(image, index) in descriptionImages" :key="index">
-        <img
-          class="company-description__image-item"
-          width="300"
-          height="232"
-          :src="require(`~/assets/images/${image}.jpg`)"
-          alt="фото команды"
-        >
-      </li>
-    </ul>
+    <div class="company-description__info-wrapper">
+      <div>
+        <p class="company-description__offer">Читайте нас в VK</p>
+        <span class="company-description__about">работаем более 5 лет</span>
+        <hr class="company-description__divider">
+      </div>
+      <ul class="company-description__images-list">
+        <li v-for="(image, index) in descriptionImages" :key="index">
+          <img
+            class="company-description__image-item"
+            :width="isDesktopLayout ? 205 : 300"
+            :height="isDesktopLayout ? 158 : 232"
+            :src="require(`~/assets/images/${image}.jpg`)"
+            alt="фото команды"
+          >
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
+  props: {
+    isDesktopLayout: {
+      type: Boolean,
+      required: true
+    }
+  },
   data: () => ({
     descriptionImages: [
       'description-1',
@@ -75,25 +84,29 @@ export default {
     color: #00f2ff;
   }
 
+  .company-description__content-wrapper {
+    display: grid;
+    row-gap: 32px;
+    row-gap: 40px;
+    margin-bottom: 50px
+  }
+
   .company-description__definition {
     font-size: 14px;
     line-height: 24px;
     font-weight: 700;
     letter-spacing: -0.5px;
-    margin-bottom: 40px;
   }
 
   .company-description__story {
     font-size: 12px;
     line-height: 20px;
     font-weight: 500;
-    margin-bottom: 40px;
   }
 
   .company-description__features-list {
     display: grid;
     row-gap: 34px;
-    margin-bottom: 50px;
     margin-left: 25px;
   }
 
@@ -115,6 +128,11 @@ export default {
     border-right: 7px solid transparent;
     border-top: 12px solid #01f1fe;
     border-bottom: 0;
+  }
+
+  .company-description__info-wrapper {
+    display: grid;
+    row-gap: 30px;
   }
 
   .company-description__offer {
@@ -147,17 +165,64 @@ export default {
     background-color: white;
     border: none;
     margin-top: 15px;
-    margin-bottom: 30px;
   }
 
   .company-description__images-list {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 17px;
+    display: grid;
+    row-gap: 17px;
   }
 
   .company-description__image-item {
     width: 300px;
     height: 232px;
+  }
+
+  @media (min-width: 1218px) {
+    .company-description {
+      padding: 60px 40px 0px 130px;
+    }
+
+    .company-description__heading {
+      font-size: 40px;
+      line-height: 48px;
+    }
+
+    .company-description__content-wrapper {
+      grid-template-columns: 540px auto;
+      row-gap: 32px;
+      column-gap: 70px;
+      margin-bottom: 80px;
+    }
+
+    .company-description__definition {
+      font-size: 17px;
+      line-height: 26px;
+      margin-bottom: 30px;
+    }
+
+    .company-description__story {
+      font-size: 13px;
+      line-height: 22px;
+      grid-column-start: 1;
+    }
+
+    .company-description__features-list {
+      row-gap: 34px;
+      grid-column-end: -1;
+    }
+
+    .company-description__info-wrapper {
+      grid-template-columns: 1fr auto;
+    }
+
+    .company-description__images-list {
+      display: flex;
+      column-gap: 22px;
+    }
+
+    .company-description__image-item {
+      width: 205px;
+      height: 158px;
+    }
   }
 </style>
